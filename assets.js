26de1c1a53076dfe795e8fe414a93731d797133d
YoshuaNava <yoshua.nava.chocron@gmail.com>
2015-12-22 18:32:16 -0430

    Polishing carte presentation for projects and blog posts.

--- assets.js
+++ assets.js
@@ -9680,38 +9680,39 @@ $('article').each(function () {
 var anchor = window.location.hash.substring(1);
 if (anchor) $('article a[name="' + anchor + '"]').trigger('click');
 
-// Expanding the article on link click and scrolling down to it
-$('#sidebar a').each(function () {
-    var that = $(this);
-    var id = that.attr('href').substring(1);
-    that.click(function (e) {
-        var header = $('article a[name="'+ id +'"]')
-        if (!header.parent().hasClass('active')) header.trigger('click');
-        $('html, body').animate({ scrollTop: header.offset().top }, 'fast');
-    });
 
-    // If we find a link in the body with similar anchor, add the same behavior
-    $('.body a[href="#'+ id +'"]').click(function (e) {
-        $('#sidebar a[href="#'+ id +'"]').trigger('click');
-    });
+
+var hide = $('<a class=\'control show\'>Hide all</a>');
+hide.click(function () {
+  $('#content article.active > a').trigger('click');    
 });
+// $('#content').prepend(hide);
 
-// Hide all/Show all links
 var show = $('<a class=\'control show\'>Show all</a>');
 show.click(function () {
   $('#content article:not(".active") > a').trigger('click');    
 });
-$('#content').prepend(show);
+// $('#content').prepend(show);
 
-var hide = $('<a class=\'control hide\'>Hide all</a>');
-hide.click(function () {
-  $('#content article.active > a').trigger('click');    
-});
-$('#content').prepend(hide);
+
+var table = $('<table>');
+table.append('<tr>');
+table.append('<td>');
+table.append(show);
+table.append('</td>');
+table.append('<td>');
+table.append(hide);
+table.append('</td>');
+table.append('</tr>');
+table.append('</table>');
+
+$('#content').prepend(table);
+
 
 // Making our navigation sticky
 new Filter($('#sidebar > ul'));
 });
+
 require.alias("boot/index.js", "carte/deps/boot/index.js");
 
 require.alias("component-jquery/index.js", "boot/deps/jquery/index.js");
